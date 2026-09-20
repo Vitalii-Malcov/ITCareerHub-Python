bbbb0cf6a55d0a95848d57beb6c9b7638e7d1c29
Vitalii Malcov <vitalii.malcov.dev@gmail.com>
2026-09-20 12:42:47 +0200

    Squashed 'Flask/Project_work/' content from commit 755b790
    git-subtree-dir: Flask/Project_work
    git-subtree-split: 755b790b02955b14c7dd682b788230ba23dcf808

---
+++ app/models/questions.py
@@ -0,0 +1,21 @@
+from app.models import db
+
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+
+    id: db.Mapped[int] = db.mapped_column(primary_key=True)
+    text: db.Mapped[str] = db.mapped_column(db.String(255))
+    category_id: db.Mapped[int | None] = db.mapped_column(
+        db.ForeignKey('categories.id', name='fk_questions_category_id')
+    )
+
+    category: db.Mapped["Category | None"] = db.relationship(
+        back_populates="questions"
+    )
+    answers: db.Mapped[list["Answer"]] = db.relationship(
+        back_populates="question", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"<Question {self.id}: {self.text}>"
